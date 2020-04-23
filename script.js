d7cfd04d1ad22e81bc12477f68e23a0caf3aac22
TUF FX <sergiu.marii2001@gmail.com>
2020-04-23 17:33:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let userName = 'Sergiu';
+alert (userName);
+console.log (typeof userName);
+
+var num = 128;
+console.log (typeof num);
+
+var bool = true;
+console.log (typeof bool);
+
+var object=({name:'sergiu',age: 18});
+console.log (typeof object);
+
+ underfined = undefined;
+console.log (typeof undefined);
+
+console.log( null)
+
+const bigint = 99999n;
+console.log (typeof bigint);
+
+var hey2 = null;
+console.log (typeof hey2);
